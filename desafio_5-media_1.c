628287c35c64d05e4cbf3a02d1fe1a94c0c8bfb4
magaum <weslei_luiz22@hotmail.com>
2016-09-04 14:10:44 -0300

    Create desafio_5-media_1.c

---
+++ desafio_5-media_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+	double c, c1, MEDIA_TOTAL;
+
+    scanf("%lf", & c);
+		scanf("%lf", & c1);
+     MEDIA_TOTAL = (((c * 3.5)+(c1 * 7.5))/11);
+		printf("MEDIA = %.5f\n", MEDIA_TOTAL);
+
+	return 0;
+}
