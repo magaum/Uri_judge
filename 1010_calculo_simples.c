0ef79373c17366d99a6bb4bd217dc7fa609d0000
magaum <weslei_luiz22@hotmail.com>
2016-09-04 18:17:31 -0300

    Create 1010_calculo_simples.c

---
+++ 1010_calculo_simples.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+	int codigo_peca_1, numero_peca_1, codigo_peca_2, numero_peca_2;
+	float valor_peca_1, valor_peca_2, total;
+		scanf ("%d %d %f", & codigo_peca_1, & numero_peca_1, & valor_peca_1);
+		scanf ("%d %d %f", & codigo_peca_2, & numero_peca_2, & valor_peca_2);
+			total = ((valor_peca_1 * numero_peca_1) + (valor_peca_2 * numero_peca_2));
+	printf("VALOR A PAGAR: R$ %.2f\n", total);
+	return 0;
+}
