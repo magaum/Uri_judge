bed3f405a877c00f627b937090382020aee4429b
magaum <weslei_luiz22@hotmail.com>
2016-09-04 17:53:26 -0300

    Create 1008_salario.c

---
+++ 1008_salario.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+	int numero_funcionario, horas_trabalhadas;
+	float recebimento_por_hora, salario;
+		scanf("%d",& numero_funcionario);
+		scanf("%d",& horas_trabalhadas);
+		scanf("%f",& recebimento_por_hora);
+			salario = (horas_trabalhadas * recebimento_por_hora);
+	printf("NUMBER = %d\nSALARY = U$ %.2f\n", numero_funcionario, salario);
+	return 0;
+
+}
