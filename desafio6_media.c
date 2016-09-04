14185af503e4a81f38aa534feeb6c2ca25bb2f4a
magaum <weslei_luiz22@hotmail.com>
2016-09-04 16:28:13 -0300

    Create desafio6_media.c

---
+++ desafio6_media.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+	double nota_1, nota_2, nota_3, MEDIA_TOTAL;
+
+    scanf("%lf", & nota_1);
+		scanf("%lf", & nota_2);
+		scanf("%lf", & nota_3);
+     MEDIA_TOTAL = ( ( (( (nota_1 * 2) + (nota_2 * 3)) + (nota_3 * 5)) )/10);
+		printf("MEDIA = %.1f\n", MEDIA_TOTAL);
+
+	return 0;
+}
