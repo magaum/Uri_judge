bddeea31ba4887d3ca8dbcaa7ca7be645dc9e1ff
magaum <weslei_luiz22@hotmail.com>
2016-09-04 17:53:55 -0300

    Create 1009_salario_com_bonus.c

---
+++ 1009_salario_com_bonus.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+	char nome;
+	double salario_fixo, vendas_efetuadas, total_mes;
+		scanf("%s", & nome);
+		scanf("%lf", & salario_fixo);
+		scanf("%lf", & vendas_efetuadas);
+		total_mes = ((vendas_efetuadas * 0.15) + salario_fixo);
+	printf ("TOTAL = R$ %.2lf\n", total_mes);
+	return 0;
+
+}
