3d692d6afa241018f81203cb498dd5c85c12e03b
magaum <weslei_luiz22@hotmail.com>
2016-09-06 15:01:54 -0300

    Update 1013_maior.c

--- 1013_maior.c
+++ 1013_maior.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
 
+int main () {
+  
+  int maiorAB, a,b,abs;
+    scanf("%d %d %d", &a ,&b ,&abs);
+      maiorAB = (a+b+abs(a-b))/2;
+    printf ("%d eh o maior", maiorAB);
+  return 0;
+}
