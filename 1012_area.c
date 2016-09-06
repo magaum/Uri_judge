1d0d464d25832c43a5908d39f20526ff737558b6
magaum <weslei_luiz22@hotmail.com>
2016-09-06 00:23:07 -0300

    Create 1012_area.c

---
+++ 1012_area.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+
+	double a, b, c, TRIANGULO, CIRCULO, TRAPEZIO, QUADRADO, RETANGULO;
+		scanf("%lf %lf %lf", & a, & b, & c);
+		
+		TRIANGULO = a * c / 2; 
+		CIRCULO = (3.14159 * c *c); 
+		TRAPEZIO = ((a + b) * c) / 2; 
+		QUADRADO = b * b;
+		RETANGULO = a * b;
+	
+	printf ("TRIANGULO: %.3lf\nCIRCULO: %.3lf\nTRAPEZIO: %.3lf\nQUADRADO: %.3lf\nRETANGULO: %.3lf\n", TRIANGULO, CIRCULO, TRAPEZIO, QUADRADO, RETANGULO);
+	
+	return 0;
+}
