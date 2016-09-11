9a66aa3db56ac4bc1564356862b3c9d728efc87f
magaum <weslei_luiz22@hotmail.com>
2016-09-11 20:45:46 -0300

    1015 / resolvido / BIRL

---
+++ 1015_Distancia_entre_dois_pontos.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) { 
+
+	float x1, y1, x2, y2, potencia1, potencia2, total;
+
+		scanf("%f %f", & x1, & y1);
+		scanf("%f %f", & x2, & y2);
+			potencia1 = (x1-x2);
+			potencia1 = pow(potencia1,2);
+			potencia2 = (y2-y1);
+                        potencia2 = pow(potencia2,2);
+			total = sqrt(potencia1 + potencia2);
+		printf("%.4f\n", total);
+	return 0;
+
+}
