f7dd1dc9e3a653dd63ca7beddabafd049093da14
magaum <weslei_luiz22@hotmail.com>
2016-09-11 21:56:08 -0300

    1017_gasto_combustivel.c

---
+++ 1017_gasto_combustivel.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+
+	int tempo, velocidade;
+	float distancia;
+
+		scanf("%d",& tempo); 
+		scanf("%d",& velocidade);
+
+			distancia = (tempo * velocidade) / 12.0;
+		printf("%.4f\n", distancia);
+	return 0;
+}
