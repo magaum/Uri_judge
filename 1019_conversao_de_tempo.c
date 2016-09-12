2bc47d7af5a87ef7242477b38743a7e0ab41999f
magaum <weslei_luiz22@hotmail.com>
2016-09-12 16:41:23 -0300

    Update 1019_conversao_de_tempo.c

--- 1019_conversao_de_tempo.c
+++ 1019_conversao_de_tempo.c
@@ -8,18 +8,20 @@ main () {
 	int tempo_segundos = 0;
 	int acumulador = 0;
 	
-		scanf("%d", & tempo_digitado);
-			while (tempo_digitado > 3600){
-				acumulador = tempo_digitado - 3600;
-				tempo_digitado = acumulador;
-				tempo_horas += 1;
+	scanf("%d", & tempo_digitado);
+	
+		while (tempo_digitado > 3600){
+			acumulador = tempo_digitado - 3600;
+			tempo_digitado = acumulador;
+			tempo_horas += 1;
+		}
 		
-}
-			while (tempo_digitado > 60) {
-				acumulador = tempo_digitado - 60;
-				tempo_digitado = acumulador;
-				tempo_minutos += 1;
-}			tempo_segundos = tempo_digitado;
+		while (tempo_digitado > 60) {
+			acumulador = tempo_digitado - 60;
+			tempo_digitado = acumulador;
+			tempo_minutos += 1;
+		}
+	tempo_segundos = tempo_digitado;
 		printf ("%d:%d:%d\n", tempo_horas, tempo_minutos, tempo_segundos);
 	return 0;
 }
