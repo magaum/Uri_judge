cde77404b2bd327e57f9299fcee604b16df47d7d
magaum <weslei_luiz22@hotmail.com>
2016-09-17 20:14:05 -0300

    Create programa_aleatório_incabado.c

---
+++ programa_aleatório_incabado.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+
+	int numero_testes, N1, D1, N2, D2, soma, subtracao, multiplicacao, divisao, lista[], posicao_vetor;
+	
+		scanf ("%d", & numero_testes);
+		
+		for (contador = 0; contador < numero_testes;contador++)
+			scanf ("%d" "%d" "%d" "%d", & lista[posicao_vetor],posicao_vetor + 1, & lista[posicao_vetor],posicao_vetor + 1, & lista[posicao_vetor],posicao_vetor + 1, & lista[posicao_vetor]posicao_vetor + 1);
+		
+		soma = ((N1*D2+N2*D1)/(D1*D2));
+		subtracao = ((N1*D2 - N2*D1)/(D1*D2));
+		multiplicacao = ((N1*N2)/(D1*D2));
+		divisao = ((N1/D1)/(N2/D2));
+	
