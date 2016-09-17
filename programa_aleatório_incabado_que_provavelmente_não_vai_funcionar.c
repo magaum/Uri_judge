3aac2c65eef99934d9d733ef46c02a92860c9b60
magaum <weslei_luiz22@hotmail.com>
2016-09-17 20:26:05 -0300

    Update programa_aleatório_incabado_que_provavelmente_não_vai_funcionar.c

--- programa_aleatório_incabado_que_provavelmente_não_vai_funcionar.c
+++ programa_aleatório_incabado_que_provavelmente_não_vai_funcionar.c
@@ -6,11 +6,25 @@ int main () {
 	
 		scanf ("%d", & numero_testes);
 		
-		for (contador = 0; contador < numero_testes;contador++)
+		for (contador = 0; contador < numero_testes;contador++){
 			scanf ("%d" "%d" "%d" "%d", & lista[posicao_vetor],posicao_vetor + 1, & lista[posicao_vetor],posicao_vetor + 1, & lista[posicao_vetor],posicao_vetor + 1, & lista[posicao_vetor]posicao_vetor + 1);
-		
-		soma = ((N1*D2+N2*D1)/(D1*D2));
-		subtracao = ((N1*D2 - N2*D1)/(D1*D2));
-		multiplicacao = ((N1*N2)/(D1*D2));
-		divisao = ((N1/D1)/(N2/D2));
+/*		
+			if (lista == +){
+				soma = ((N1*D2+N2*D1)/(D1*D2));	
+			}
+			if (lista == - ){
+				subtracao = ((N1*D2 - N2*D1)/(D1*D2));
+			}
+			if (lista == *){
+				multiplicacao = ((N1*N2)/(D1*D2));
+			}
+			if (lista == /){
+				divisao = ((N1/D1)/(N2/D2));	
+			}
+			
+preciso estudar sei nem o que queria fazer nisso aqui
+*/
+}
+	
+
 	
