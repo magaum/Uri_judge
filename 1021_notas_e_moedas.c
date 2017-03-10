fe771d1eb3d5f3cdf94c3d4c8fac368d886ff6d4
magaum <weslei_luiz22@hotmail.com>
2017-03-10 09:32:42 -0300

    Update 1021_notas_e_moedas.c
    Correção do exercício após muito tempo :')

--- 1021_notas_e_moedas.c
+++ 1021_notas_e_moedas.c
@@ -1,72 +1,78 @@
 #include <stdio.h>
 
-int main () {
+//Accepted
 
-	double valor;
-	float acumulador, contador100, contador50, contador20, contador10, contador5, contador2, contador1, contador0_50, contador0_25, contador0_10, contador0_05, contador0_01;
-		scanf("%lf", & valor);
+int main (){
 
-			for (contador100 = 0; valor >= 100.0; contador100 ++) {
-				acumulador = valor - 100.0;
-				valor = acumulador;
-				
-				}
-			for (contador50 = 0; valor >= 50.0; contador50 ++) {
-				acumulador = valor - 50.0;
-               			valor = acumulador;
+	int nota_100 = 0, nota_50 = 0, nota_20 = 0, nota_10 = 0, nota_5 = 0, nota_2 = 0, moeda_1 = 0, moeda_50 = 0, moeda_25 = 0, moeda_10 = 0, moeda_5 = 0, moeda_0_1 = 0;
+	double numero;
 
-				}
-			for (contador20 = 0; valor >= 20.0; contador20 ++) {
-				acumulador = valor - 20.0;
-				valor = acumulador;
-				
-				}
-			for (contador10 = 0; valor >= 10.0; contador10 ++) {
-				acumulador = valor - 10.0;
-				valor = acumulador;
-				
-				}
-			for (contador5 = 0; valor >= 5.0; contador5 ++) {
-				acumulador = valor - 5.0;
-				valor = acumulador;
+	scanf("%lf", &numero);
 
-				}
-			for (contador2 = 0; valor >= 2.0; contador2 ++) {
-				acumulador = valor - 2.0;
-		      	
-				}
-			for (contador1 = 0; valor >= 1.0; contador1 ++) {
-				acumulador = valor - 1.0;
-				valor = acumulador;
-				
-				}
-			for (contador0_50 = 0; valor >= 0.50; contador0_50 ++) {
-				acumulador = valor - 0.50;
-				valor = acumulador;
-				
-				}
-			for (contador0_25 = 0; valor >= 0.25; contador0_25 ++) {
-				acumulador = valor - 0.25;
-				valor = acumulador;
-				
-				}
-			for (contador0_10 = 0; valor >= 0.10; contador0_10 ++) {
-				acumulador = valor - 0.10;
-				valor = acumulador;
-				
-				}
-			for (contador0_05 = 0; valor >= 0.05; contador0_05 ++) {
-				acumulador = valor - 0.05;
-		    		valor = acumulador;
-				
-				}
-			for (contador0_01 = 0; valor >= 0.01; contador0_01 ++) {
-				acumulador = valor - 0.01;
-		    		valor = acumulador;
-				
-				}
-		printf ("NOTAS:\n%d nota(s) de R$ 100.00\n%d nota(s) de R$ 50.00\n%d nota(s) de R$ 20.00\n%d nota(s) de R$ 10.00\n%d nota(s) de R$ 5.00\n%d nota(s) de R$ 2.00\nMOEDAS:\n%d MOEDAS(s) de R$ 1.00\n%d MOEDAS(s) de R$ 0.50\n%d MOEDAS(s) de R$ 0.25\n%d MOEDAS(s) de R$ 0.10\n%d MOEDAS(s) de R$ 0.05\n%d MOEDAS(s) de R$ 0.01\n", contador100, contador50, contador20, contador10, contador5, contador2, contador1, contador0_50, contador0_25, contador0_10, contador0_05, contador0_01);	
+	if (numero >= 100){
+		nota_100 = numero / 100;
+		numero = numero - (nota_100 * 100.0);
+	}
+
+	if (numero >= 50){
+		nota_50 = numero / 50;
+		numero  = numero - (nota_50 * 50.0);
+	}
+	
+	if (numero >= 20){
+		nota_20 = numero / 20;
+		numero -= (nota_20 * 20.0);
+	}
+
+	if (numero >= 10){
+		nota_10 = numero / 10;
+		numero -= (nota_10 * 10.0);
+	}
+	
+	if (numero >= 5){
+		nota_5 = numero / 5;
+		numero -= (nota_5 * 5.0);
+	}
+
+	if (numero >= 2){
+		nota_2 = numero / 2;
+		numero -= (nota_2 * 2.0);
+	}
+
+	numero *= 100;
+
+	if (numero >= 100){
+		moeda_1 = numero / 100;
+		numero -= (moeda_1 * 100);
+	}
+
+	if (numero >= 50){
+		moeda_50 = numero / 50;
+		numero -= (moeda_50 * 50);
+	}
+
+	if (numero >= 25){
+		moeda_25 = numero / 25;
+		numero -= (moeda_25 * 25);
+	}
+
+	if (numero >= 10){
+		moeda_10 = numero / 10;
+		numero -= (moeda_10 * 10);
+	}
+
+	if (numero >= 5){	
+		moeda_5 = numero / 5;
+		numero -= (moeda_5 * 5);
+	}
+	
+	if (numero >= 1) {
+		moeda_0_1 = numero;
+	}
+
+	printf("NOTAS:\n%d nota(s) de R$ 100.00\n%d nota(s) de R$ 50.00\n%d nota(s) de R$ 20.00\n%d nota(s) de R$ 10.00\n%d nota(s) de R$ 5.00\n%d nota(s) de R$ 2.00\nMOEDAS:\n%d moeda(s) de R$ 1.00\n%d moeda(s) de R$ 0.50\n%d moeda(s) de R$ 0.25\n%d moeda(s) de R$ 0.10\n%d moeda(s) de R$ 0.05\n%d moeda(s) de R$ 0.01\n", nota_100, nota_50, nota_20, nota_10, nota_5, nota_2, moeda_1, moeda_50, moeda_25, moeda_10, moeda_5, moeda_0_1);
+
+	
 	return 0;
+	
 }
-
-//Time limit exceeded
